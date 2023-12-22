3788dc576950f5c87d188316e83b99cc0109e943
RobotBot5 <83837203+RobotBot5@users.noreply.github.com>
2023-12-22 21:51:58 +0300

    Delete scripts/script_about.js

--- scripts/script_about.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.video-container').addEventListener('click', function() {
-    this.classList.toggle('tapped');
-    let video = this.querySelector('video');
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-});
